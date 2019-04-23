16748215a9672ed2f283a89b92e0fa7e5ec20358
Chris Castano <cdc67@georgetown.edu>
2019-04-22 20:10:53 -0400

    Final commit

--- server.R
+++ server.R
@@ -13,53 +13,52 @@ shinyServer(function(input, output) {
    whobardata = data2 %>% 
       filter(Paying == input$spender, year == input$year1) %>% 
       group_by(Paid) %>% 
-      summarise(total = sum(amount, na.rm = TRUE)) %>% 
-      filter(total > 5000)
-    gvisColumnChart(whobardata, options = list(vAxes="[{viewWindowMode:'explicit',
-			viewWindow:{min:0, max:400000}}]"))
+      summarise(TotalUSD = sum(amount, na.rm = TRUE)) %>% 
+      filter(TotalUSD > 5000)
+    gvisColumnChart(whobardata, options = list(legend = "none", vAxes="[{title:'Total Amount Paid (USD)'}]",
+                            hAxes="[{title:'Organization Paid'}]", height = 500))
   })
   
   output$whatbar = renderGvis({
     whatbardata = data2 %>% 
       filter(Paying == input$spender, year == input$year1) %>% 
       group_by(purpose_scrubbed) %>% 
-      summarise(total = sum(amount, na.rm = TRUE))
-    gvisColumnChart(whatbardata, options = list(vAxes="[{viewWindowMode:'explicit',
-			viewWindow:{min:0, max:400000}}]"))
+      summarise(TotalUSD = sum(amount, na.rm = TRUE))
+    gvisColumnChart(whatbardata, options = list(legend = "none", vAxes="[{title:'Total Amount Paid (USD)'}]",
+                                                hAxes="[{title:'Payment Category'}]", height = 500))
   })
   
-  #output$wherebar = renderGvis({
-   # wherebardata = data2 %>% 
-    #  filter(Paying == input$spender, year == input$year1) %>% 
-     # group_by(state) %>% 
-      #summarise(total = sum(amount, na.rm = TRUE))
-  #  gvisColumnChart(wherebardata, options = list(vAxes="[{viewWindowMode:'explicit',
-		#	viewWindow:{min:0, max:400000}}]"))
-  #})
+  output$wherebar = renderGvis({
+    wherebardata = data2 %>% 
+      filter(Paying == input$spender, year == input$year1) %>% 
+      group_by(state) %>% 
+      summarise(TotalUSD = sum(amount, na.rm = TRUE))
+      gvisColumnChart(wherebardata, options = list(title = 'State Totals', legend = "none", vAxes="[{title:'Total Amount Paid (USD)'}]",
+                                                   hAxes="[{title:'U.S. State'}]", width = 800, height = 300))
+  })
 
+  
   #output$wheremap = renderGvis({
-   # wherebarmapdata = data2 %>% 
-    #  filter(Paying == input$spender, year == input$year1) %>% 
-     # group_by(state) %>% 
-    #  summarise(total = sum(amount, na.rm = TRUE))
-    #gvisGeoChart(wherebarmapdata, "state", "total", options=list(region="US", 
-     #                                                       displayMode="regions", 
-      #                                                      resolution="provinces"))
+  #  wherebarmapdata = data2 %>% 
+  #    filter(Paying == input$spender, year == input$year1) %>% 
+  #    group_by(state) %>% 
+     # summarise(TotalUSD = sum(amount, na.rm = TRUE))
+    #plot.title = "State Totals"
+   # gvisGeoChart(wherebarmapdata, "state", "TotalUSD", options=list(title = plot.title, region="US", 
+                                                           # displayMode="regions", 
+                                                            #resolution="provinces", height = 400))
   #})
   
   output$googmap = renderGvis({
     googwheremapdata = data3 %>%
       filter(Paying == input$spender, year == input$year1) %>% 
       group_by(Paid) %>% 
-      summarise(total = sum(amount, na.rm = TRUE), llong = first(latlong.y))
-    gvisMap(googwheremapdata, "llong" , "total", 
+      summarise(TotalUSD = sum(amount, na.rm = TRUE), llong = first(latlong.y)) %>% 
+      mutate(finalStr = paste(Paid,as.character(TotalUSD), sep = ": $"))
+    gvisMap(googwheremapdata, "llong" , "finalStr", 
             options=list(showTip=TRUE, 
-                         showLine=TRUE, 
                          enableScrollWheel=TRUE,
                          mapType='terrain', 
-                         useMapTypeControl=TRUE))
+                         useMapTypeControl=TRUE, height = 300))
   })
 })
-
-
-
--- ui.R
+++ ui.R
@@ -11,35 +11,49 @@ shinyUI(dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Introduction", tabName = "introtab", icon = icon("newspaper")),
-      selectizeInput("spender", "Select Political Entity:", choices = spenders),
+      selectizeInput("spender", "Select Political Organization:", choices = spenders),
       selectizeInput("year1", "Select Year:", choices = 2015:2018),
       menuItem("WHO DID THEY PAY?", tabName = "whotab", icon = icon("user")),
       menuItem("WHAT DID THEY PAY FOR?", tabName = "whattab", icon = icon("money-bill-wave")),
-      menuItem("WHERE DID THEY PAY?", tabName = "wheretab", icon = icon("map"))
+      menuItem("WHERE DID THEY PAY?", tabName = "wheretab", icon = icon("map")),
+      sidebarMenuOutput("menu")
       )
   ),
   dashboardBody(
     tabItems(
-      tabItem(tabName = "introtab", box(h2("Political Spending at Trump-owned Properties by Entity, Year", align = 'center'), 
-                                    h4("Coded by: Chris Castano (cdc67@georgetown.edu)", align = 'center'),
+      tabItem(tabName = "introtab", h2(strong("Political Spending at Trump-owned Properties by Entity, Year"), align = 'center'), 
+                                    
+                                    h4(strong("Coded by: Chris Castano (cdc67@georgetown.edu)"), align = 'center'),
+                                    
+                                    img(src = "http://static.digg.com/images/6a4a5c6a1f404b3da2141eaec0860206_9733b849c381477383942c1516d20f9d_header.jpeg", 
+                                        align = 'center'),
+                                    
                                     p(''),
-                                    p(''),
-                                    h1(strong("Political campaigns and government agencies have spent at least $16,085,911.67 at 
-                                              properties owned by the president since 2015."), align = 'center'),
-                                    p(''),
-                                    p(''),
-                                    p('This app generates basic visualizations of political spending at Trump-owned properties in the United States 
-                                      based on a selected year and organization. Only the 15 heaviest spending organizations are available at the moment.'),
-                                    p(''),
+                                    
+                                    box(h1(strong("Political campaigns and government agencies have spent at least $16,085,911.67 at 
+                                              properties owned by the president since 2015."), align = 'center')),
+      
+                                    p("This app generates basic visualizations of political spending at Trump-owned properties in the United States 
+                                      based on a selected year and organization. Use the drop down menus to change what organization's spending 
+                                      you're examining and when that spending occured. Only the 15 heaviest spending organizations are available at the moment 
+                                      for the sake the visuals. Many agencies or campaigns have made small purchases. The smallest purchases have been 
+                                      filtered from some charts to make them easier to read."),
+                                    
                                     p('The data used for this app is free to download from the ProPublica Data Store. 
                                       I do not own it. I have not modified its content significantly except to make visualization easier. 
-                                      This version of the data was accessed April 12, 2019'),
-                                    p(''),
+                                      This version of the data was accessed April 12, 2019.'),
+                                    
+                                    p('To download the data for yourself visit: '),
+                                    
+                                    p(a("https://www.propublica.org/datastore/dataset/spending-at-trump-properties", href = 'https://www.propublica.org/datastore/dataset/spending-at-trump-properties')),
+                                    
                                     strong("ProPublica's notes:"),
+                                    
                                     em("Please note: Federal government spending is incomplete because many government 
                                       agencies have actively fought requests to disclose spending at Trump properties. 
                                       The data we have so far was released, in part, after lawsuits. We'll continue to update 
                                       this page as we receive more data."),
+                                    
                                     em("Federal Election Commission data is from April 30, 2015 through May 8, 2018; 
                                        Federal agency expenditure data from the Department of Commerce is from Jan. 15, 2017 
                                        through April 10, 2018; data from the Department of Defense is from Jan. 20, 2017 through 
@@ -47,16 +61,24 @@ shinyUI(dashboardPage(
                                        Feb. 13, 2018; data from the General Services Administration is from Jan. 20, 2017 through 
                                        Nov. 20, 2017; data from the Department of State is from Jan. 20, 2017 through Aug. 2, 2017, 
                                        with three expenditures with unknown transaction dates; State and local government agency 
-                                       expenditure data is from Jan. 20, 2017 through June 2018."))
+                                       expenditure data is from Jan. 20, 2017 through June 2018.")
               ),
       
-      tabItem(tabName = "whotab", h3("What Trump-owned company was this entity paying?"), fluidRow(box(htmlOutput("whobar")))),
-      tabItem(tabName = "whattab", h3("What types of services was this entity paying for?"), fluidRow(box(htmlOutput("whatbar")))),
-      tabItem(tabName = "wheretab", h3("Where was this spender making this payment?"), 
-              #fluidRow(box(htmlOutput("wherebar"))), 
+      tabItem(tabName = "whotab", 
+              h3(strong("What Trump-owned company was this organization paying?"), align = 'center'), 
+              fluidRow(htmlOutput("whobar"), height = 400),
+              h4("Note: If chart does not appear, organization did not make a purchase in this year.")),
+      tabItem(tabName = "whattab", 
+              h3(strong("What types of services was this organizations paying for?"), align = 'center'), 
+              fluidRow(htmlOutput("whatbar")), 
+              h4("Note: If chart does not appear, organization did not make a purchase in this year.")),
+      tabItem(tabName = "wheretab", 
+              h3(strong("Where was this organization making this payment?"), align = 'center'), 
+              fluidRow((htmlOutput("googmap"))), 
               fluidRow(
-                #box(htmlOutput("wheremap")), 
-                (htmlOutput("googmap"))))
+                htmlOutput("wherebar"), align = 'center'
+                ),
+              h4("Note: If chart does not appear, organization did not make a purchase in this year."))
 ))))
 
 
